4c7bcc1d7fbc9072eb1662b3ff55eed407cc4cad
theoroiroi <114597785+theoroiroi@users.noreply.github.com>
2022-09-28 17:46:16 +0200

    Add files via upload

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Text ZahlText;
+    private int score;
+
+    void Start()
+    {
+        ZahlText.text = "";
+    }
+
+    void Update()
+    {
+        ZahlText.text = "" + score;
+    }
+
+    public void Plus()
+    {
+        score = score + 1;
+    }
+
+    public void Minus()
+    {
+        score = score - 1;
+    }
+
+    public void Plus10()
+    {
+        score = score + 10;
+    }
+
+    public void Minus10()
+    {
+        score = score - 10;
+    }
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Multiply()
+    {
+        score = score * 2;
+    }
+
+    public void Divide()
+    {
+        score = score / 2;
+    }
+
+    public void Reset()
+    {
+        score = 0;
+    }
+
+
+}
---
+++ Assets/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    public void SceneSwitch()
+    {
+        Application.LoadLevel(1);
+    }
+
+}
